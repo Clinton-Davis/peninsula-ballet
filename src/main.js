0b22d94aed5045a5edcbe85919728ae8c67ac315
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-01-26 16:09:18 +0000

    added all router files to router.js and works correctly

--- src/main.js
+++ src/main.js
@@ -1,33 +1,8 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import { init } from "emailjs-com";
 import App from "./App.vue";
-import WelcomePage from "./components/pages/WelcomePage.vue";
-import Schedule from "./components/pages/Schedule.vue";
-import Attire from "./components/pages/Attire.vue";
-import Covid from "./components/pages/Covid.vue";
-import Enrolment from "./components/pages/Enrolment.vue";
-import Studios from "./components/pages/Studios.vue";
-import Teachers from "./components/pages/TeacherPage/Teachers.vue";
-import EventList from "./components/pages/EventsPage/EventList.vue";
-import TheEvent from "./components/pages/EventsPage/TheEvent.vue";
+import router from "./router";
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/", redirect: "/welcome" },
-    { path: "/welcome", component: WelcomePage },
-    { path: "/teachers", component: Teachers },
-    { path: "/schedule", component: Schedule },
-    { path: "/covid", component: Covid },
-    { path: "/enrolment", component: Enrolment },
-    { path: "/studios", component: Studios },
-    { path: "/eventlist", component: EventList },
-    { path: "/eventlist/:eventId", component: TheEvent, props: true },
-    { path: "/attire", component: Attire },
-    { path: "/:notFound(.*)", component: WelcomePage },
-  ],
-});
 init("user_3B9k5oVXE8HXYgesuRXAt");
 const app = createApp(App);
 app.use(router);
