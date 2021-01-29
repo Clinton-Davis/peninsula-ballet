2590832afaa8660761a14af5322c98804d2ec8ca
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-01-29 19:43:15 +0000

    added vuex and index.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import App from "./App.vue";
 import { createApp } from "vue";
 import { init } from "emailjs-com";
 import router from "./router";
+// import store from "./store/index.js";
 
 init("user_3B9k5oVXE8HXYgesuRXAt");
 const app = createApp(App);
-
+// app.use(store);
 app.use(router);
 app.mount("#app");
