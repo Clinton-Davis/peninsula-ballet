a0edd1b74ceea28c5ae93724d698fb0b28c875b7
Clinton-Dais <clintongadavis@gmail.com>
2022-01-30 13:50:35 +0000

    Studios and welcomepage change

--- src/components/pages/Studios.vue
+++ src/components/pages/Studios.vue
@@ -1,203 +1,192 @@
 <template>
-  <section id="studio__Section">
-    <div class="studio__Img" :style="studioImg">
-      <div class="studio__InfoBlock">
-        <div class="studio__InfoBlock-heading">
-          <h1>Studios</h1>
-          <h4>
-            Before arriving please familiarize with the <br />
-            <router-link id="covidLink" to="/covid"
-              >Covid Safty Regulations</router-link
-            >
-          </h4>
+    <section id="studio__Section">
+        <div class="studio__Img" :style="studioImg">
+            <div class="studio__InfoBlock">
+                <div class="studio__InfoBlock-heading">
+                    <h1>Studios</h1>
+                    <h4>
+                        Before arriving please familiarize with the <br />
+                        <router-link id="covidLink" to="/covid">Covid Safty Regulations</router-link>
+                    </h4>
+                </div>
+                <div class="studio__card-wrapper">
+                    <div v-for="studio in studios" :key="studio.id" class="studio__card" title="Go to Google Maps">
+                        <a :href="studio.link" target="_blank" rel="noopener noreferrer">
+                            <h2>{{ studio.name }}</h2>
+                            <p>{{ studio.address }}</p>
+                            <img :src="studio.mapImage" alt="Map image" />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="studio__card-wrapper">
-          <div
-            v-for="studio in studios"
-            :key="studio.id"
-            class="studio__card"
-            title="Go to Google Maps"
-          >
-            <a :href="studio.link" target="_blank" rel="noopener noreferrer">
-              <h2>{{ studio.name }}</h2>
-              <p>{{ studio.address }}</p>
-              <img :src="studio.mapImage" alt="Map image" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      studios: [
-        {
-          id: 1,
-          name: "Fish Hoek",
-          address: "St Kirans Presbyerian church 7th Ave & Recreation Rd",
-          link: "https://bit.ly/2WksRlg",
-          mapImage:
-            "https://res.cloudinary.com/peninsulaballet/image/upload/q_100/v1616494521/web_pages/Maps/FHMap_vwustf.webp",
+    export default {
+        data() {
+            return {
+                studios: [
+                    {
+                        id: 1,
+                        name: "Fish Hoek",
+                        address: "St Kirans Presbyerian church 7th Ave & Recreation Rd",
+                        link: "https://bit.ly/2WksRlg",
+                        mapImage: "https://res.cloudinary.com/peninsulaballet/image/upload/q_100/v1616494521/web_pages/Maps/FHMap_vwustf.webp",
+                    },
+                    {
+                        id: 2,
+                        name: "Kommetjie",
+                        address: "Wellness Centre in Ruby Rd",
+                        link: "https://bit.ly/36fsM7e",
+                        mapImage: "https://res.cloudinary.com/peninsulaballet/image/upload/q_100/v1616495019/web_pages/Maps/rubyMap_r1lays.webp",
+                    },
+                    // {
+                    //   id: 3,
+                    //   name: "Muizenberg Junior School",
+                    //   address: "Main Rd, Muizenberg",
+                    //   link: "https://bit.ly/396v1Nb",
+                    //   mapImage:
+                    //     "https://res.cloudinary.com/peninsulaballet/image/upload/q_100/v1616494109/web_pages/Maps/MJHSMap_ffoxbm.webp",
+                    // },
+                    {
+                        id: 4,
+                        name: "Alive Dance Studio",
+                        address: "15 Palmer Road Muizenberg",
+                        link: "https://bit.ly/3sqRkoB",
+                        mapImage:
+                            "https://res.cloudinary.com/peninsulaballet/image/upload/q_100/v1616494101/web_pages/Maps/Alive_Studios_yxvdyy.webp",
+                    },
+                ],
+                studioImg: {
+                    backgroundImage: `url(${require("@/assets/images/dancer_on_rock-min.jpg")})`,
+                },
+            };
         },
-        {
-          id: 2,
-          name: "Kommetjie",
-          address: "Wellness Centre in Ruby Rd",
-          link: "https://bit.ly/36fsM7e",
-          mapImage:
-            "https://res.cloudinary.com/peninsulaballet/image/upload/q_100/v1616495019/web_pages/Maps/rubyMap_r1lays.webp",
-        },
-        {
-          id: 3,
-          name: "Muizenberg Junior School",
-          address: "Main Rd, Muizenberg",
-          link: "https://bit.ly/396v1Nb",
-          mapImage:
-            "https://res.cloudinary.com/peninsulaballet/image/upload/q_100/v1616494109/web_pages/Maps/MJHSMap_ffoxbm.webp",
-        },
-        {
-          id: 4,
-          name: "Alive Dance Studio",
-          address: "15 Palmer Road Muizenberg",
-          link: "https://bit.ly/3sqRkoB",
-          mapImage:
-            "https://res.cloudinary.com/peninsulaballet/image/upload/q_100/v1616494101/web_pages/Maps/Alive_Studios_yxvdyy.webp",
-        },
-      ],
-      studioImg: {
-        backgroundImage: `url(${require("@/assets/images/dancer_on_rock-min.jpg")})`,
-      },
     };
-  },
-};
 </script>
 
 <style scoped>
-#studio__Section {
-  display: flex;
-  position: relative;
-  height: fit-content;
-  color: white;
-  min-height: 100vh;
-}
-.studio__Img {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  background-size: cover;
-  background-position: top;
-  position: relative;
-}
-.studio__InfoBlock {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  height: fit-content;
-  min-height: calc(100vh + 10vh);
+    #studio__Section {
+        display: flex;
+        position: relative;
+        height: fit-content;
+        color: white;
+        min-height: 100vh;
+    }
+    .studio__Img {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        background-size: cover;
+        background-position: top;
+        position: relative;
+    }
+    .studio__InfoBlock {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        height: fit-content;
+        min-height: calc(100vh + 10vh);
 
-  margin-left: auto;
-  background-color: #6e563073;
-}
-.studio__InfoBlock-heading {
-  position: relative;
-  top: 10vh;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin: 2rem auto 4rem auto;
-  width: 100%;
-}
-.studio__card-wrapper {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  flex-wrap: wrap;
-  margin: 1rem;
-}
-.studio__card {
-  display: flex;
-  flex-direction: column;
-  width: 20rem;
-  height: fit-content;
-  min-height: 16rem;
-  margin: 1rem;
-  max-width: 50%;
-  position: relative;
-  box-shadow: 0px 2px 21px -1px rgba(0, 0, 0, 0.75),
-    0px 2px 21px -1px rgba(0, 0, 0, 0.75) inset;
-  background-color: #ba9251a9;
-  border-radius: 20px;
-  -webkit-border-radius: 20px;
-  -moz-border-radius: 20px;
-  -ms-border-radius: 20px;
-  -o-border-radius: 20px;
-  transition: all 0.3s ease;
-}
-h1,
-h2,
-h4,
-p,
-a {
-  text-align: center;
-  padding: 0.4rem;
-  text-shadow: 1px 1px rgba(0, 0, 0, 0.3);
-}
-a {
-  width: 100%;
-  min-height: 16rem;
-  cursor: pointer;
-  color: inherit;
-  text-decoration: none;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-}
-#covidLink {
-  display: block;
-  padding: 0.4rem;
-  min-height: 1rem;
-  color: #ecbee9;
-}
-h1 {
-  font-size: 5rem;
-  font-family: var(--Italianno);
-  text-shadow: 1px 1px rgba(0, 0, 0, 0.8);
-}
-h2 {
-  font-size: 2rem;
-}
+        margin-left: auto;
+        background-color: #6e563073;
+    }
+    .studio__InfoBlock-heading {
+        position: relative;
+        top: 10vh;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin: 2rem auto 4rem auto;
+        width: 100%;
+    }
+    .studio__card-wrapper {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        flex-wrap: wrap;
+        margin: 1rem;
+    }
+    .studio__card {
+        display: flex;
+        flex-direction: column;
+        width: 20rem;
+        height: fit-content;
+        min-height: 16rem;
+        margin: 1rem;
+        max-width: 50%;
+        position: relative;
+        box-shadow: 0px 2px 21px -1px rgba(0, 0, 0, 0.75), 0px 2px 21px -1px rgba(0, 0, 0, 0.75) inset;
+        background-color: #ba9251a9;
+        border-radius: 20px;
+        -webkit-border-radius: 20px;
+        -moz-border-radius: 20px;
+        -ms-border-radius: 20px;
+        -o-border-radius: 20px;
+        transition: all 0.3s ease;
+    }
+    h1,
+    h2,
+    h4,
+    p,
+    a {
+        text-align: center;
+        padding: 0.4rem;
+        text-shadow: 1px 1px rgba(0, 0, 0, 0.3);
+    }
+    a {
+        width: 100%;
+        min-height: 16rem;
+        cursor: pointer;
+        color: inherit;
+        text-decoration: none;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    #covidLink {
+        display: block;
+        padding: 0.4rem;
+        min-height: 1rem;
+        color: #ecbee9;
+    }
+    h1 {
+        font-size: 5rem;
+        font-family: var(--Italianno);
+        text-shadow: 1px 1px rgba(0, 0, 0, 0.8);
+    }
+    h2 {
+        font-size: 2rem;
+    }
 
-p {
-  font-size: 1.1rem;
-}
-.studio__card:hover,
-.studio__card:active {
-  transform: scale(1.1);
-}
-img {
-  width: 98%;
-  height: auto;
-  border: 1px solid black;
-  border-radius: 20px;
-}
-@media screen and (max-width: 880px) {
-  .studio__card {
-    margin: 2rem auto 2rem auto;
-    max-width: 80%;
-  }
-}
-@media screen and (max-width: 400px) {
-  .studio__card {
-    padding: 0.2rem;
-    max-width: 98%;
-    box-shadow: 0px 2px 21px -1px rgba(0, 0, 0, 1),
-      0px 2px 21px -1px rgba(0, 0, 0, 1) inset;
-    border: 1px solid rgb(26, 26, 26);
-  }
-}
+    p {
+        font-size: 1.1rem;
+    }
+    .studio__card:hover,
+    .studio__card:active {
+        transform: scale(1.1);
+    }
+    img {
+        width: 98%;
+        height: auto;
+        border: 1px solid black;
+        border-radius: 20px;
+    }
+    @media screen and (max-width: 880px) {
+        .studio__card {
+            margin: 2rem auto 2rem auto;
+            max-width: 80%;
+        }
+    }
+    @media screen and (max-width: 400px) {
+        .studio__card {
+            padding: 0.2rem;
+            max-width: 98%;
+            box-shadow: 0px 2px 21px -1px rgba(0, 0, 0, 1), 0px 2px 21px -1px rgba(0, 0, 0, 1) inset;
+            border: 1px solid rgb(26, 26, 26);
+        }
+    }
 </style>
--- src/components/pages/WelcomePage.vue
+++ src/components/pages/WelcomePage.vue
@@ -1,238 +1,209 @@
 <template>
-  <section id="welcome__Section">
-    <div :style="cssProps" class="welcome__Img">
-      <div class="welcome__Wrap">
-        <div class="welcome__Content">
-          <h1 class="welcome__Heading text-Shadow-White">Peninsula Ballet</h1>
-          <h2 class="welcome__Tag text-Shadow-White">
-            Passionate about teaching
-          </h2>
-          <div class="welcome__celebration">
-            <img
-              class="leftFire"
-              :src="require('@/assets/images/works2.svg')"
-              alt="Fireworks icons"
-            />
-            <h3 class="textShadow text-Shadow-White">
-              Celebrating A Decade of Dance.
-            </h3>
-            <img
-              class="rightFire"
-              :src="require('@/assets/images/works2.svg')"
-              alt="Fireworks icons"
-            />
-          </div>
-          <div class="welcome__benefits">
-            <!-- <h2 class="text-Shadow-White">Welcome</h2> -->
-            <p>
-              <span class="welcome__headder"
-                >Welcome to Peninsula Ballet.
-              </span>
-              You will find all the information you need to get started here.
-              Why is it a good idea to begin ballet? <br />
-              Aside from having fun and making friends, there are so many
-              benefits to learning ballet, as it is an excellent form of
-              exercise. <br />
-              Having flexibility is essential in ballet, as it helps to prevent
-              injury and promotes muscle strength, which in turn results in good
-              posture. <br />
-              Besides being beneficial for your physical health, it is also
-              beneficial for mental wellbeing. Children learn to remain focused
-              and to learn from their mistakes, which results in perseverance
-              and a boost in self-confidence when they succeed. <br />
-              There are many other benefits that will remain with them for a
-              lifetime.
-            </p>
-          </div>
-          <div
-            class="welcome__enrolBtn text-Shadow-White"
-            title="Go to Enrolment Form"
-          >
-            <router-link
-              to="Enrolment"
-              aria-command-name="Enrolment Form button"
-              aria-label="Link to enrolment page"
-            >
-              Enrolment Form &raquo;</router-link
-            >
-          </div>
+    <section id="welcome__Section">
+        <div :style="cssProps" class="welcome__Img">
+            <div class="welcome__Wrap">
+                <div class="welcome__Content">
+                    <h1 class="welcome__Heading text-Shadow-White">Peninsula Ballet</h1>
+                    <h2 class="welcome__Tag text-Shadow-White">
+                        Passionate about teaching
+                    </h2>
+                    <div class="welcome__celebration">
+                        <h3 class="textShadow text-Shadow-White">
+                            Celebrating A Decade of Dance.
+                        </h3>
+                    </div>
+                    <div class="welcome__benefits">
+                        <!-- <h2 class="text-Shadow-White">Welcome</h2> -->
+                        <p>
+                            <span class="welcome__headder">Welcome to Peninsula Ballet. </span>
+                            You will find all the information you need to get started here. Why is it a good idea to begin ballet? <br />
+                            Aside from having fun and making friends, there are so many benefits to learning ballet, as it is an excellent form of
+                            exercise. <br />
+                            Having flexibility is essential in ballet, as it helps to prevent injury and promotes muscle strength, which in turn
+                            results in good posture. <br />
+                            Besides being beneficial for your physical health, it is also beneficial for mental wellbeing. Children learn to remain
+                            focused and to learn from their mistakes, which results in perseverance and a boost in self-confidence when they succeed.
+                            <br />
+                            There are many other benefits that will remain with them for a lifetime.
+                        </p>
+                    </div>
+                    <div class="welcome__enrolBtn text-Shadow-White" title="Go to Enrolment Form">
+                        <router-link to="Enrolment" aria-command-name="Enrolment Form button" aria-label="Link to enrolment page">
+                            Enrolment Form &raquo;</router-link
+                        >
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      cssProps: {
-        backgroundImage: `url(${require("@/assets/images/adam-walker-KRIfJDXgsiQ-unsplash.jpg")})`,
-      },
+    export default {
+        data() {
+            return {
+                cssProps: {
+                    backgroundImage: `url(${require("@/assets/images/adam-walker-KRIfJDXgsiQ-unsplash.jpg")})`,
+                },
+            };
+        },
+        methods: {},
     };
-  },
-  methods: {},
-};
 </script>
 
 <style scoped>
-#welcome__Section {
-  display: flex;
-  position: relative;
-  height: 100vh;
-}
-#welcomePara {
-  line-height: 20.5px;
-  color: rgb(51, 51, 51);
-}
-.welcome__Wrap {
-  display: flex;
-  top: 30px;
-  flex-direction: column;
-  background-image: linear-gradient(
-    to bottom right,
-    rgb(189, 235, 229),
-    rgba(255, 255, 255, 0.493)
-  );
-  max-width: 45rem;
-  max-height: auto;
-  position: relative;
-  z-index: 45;
-  margin: 2em;
-  box-shadow: 1px 20px 25px rgba(0, 0, 0, 0.3);
-  border-radius: 30px;
-  border-radius: 20px;
-  -webkit-border-radius: 20px;
-  -moz-border-radius: 20px;
-  -ms-border-radius: 20px;
-  -o-border-radius: 20px;
-}
-.welcome__Content {
-  padding: 1rem;
-  box-shadow: 0px 2px 21px -1px rgba(0, 0, 0, 0.75),
-    0px 2px 21px -1px rgba(0, 0, 0, 0.75) inset;
-  border-radius: 20px;
-}
-.welcome__Heading {
-  font-family: var(--Italianno);
-  font-size: 4em;
-  text-align: center;
-  color: black;
-}
-.welcome__Tag {
-  font-family: var(--Italianno);
-  font-size: 2.5em;
-  text-align: center;
-  margin-top: -1.5rem;
-}
-.welcome__headder {
-  font-family: var(--Italianno);
-  font-size: 2.5em;
-  margin-top: 1rem;
-  color: black;
-}
-.welcome__benefits {
-  font-family: var(--Italianno);
-  font-size: 2em;
-  text-align: center;
-}
-.welcome__benefits p {
-  font-family: var(--Roboto);
-  color: var(--lightBlack);
-  font-size: 0.5em;
-  text-align: left;
-  padding: 0 1rem;
-  margin-top: 1rem;
-  line-height: 1.55rem;
-}
-.welcome__celebration {
-  display: flex;
-  text-align: center;
-  justify-content: center;
-}
-.welcome__celebration h3 {
-  font-family: var(--Italianno);
-  font-size: 2.2em;
-  text-align: center;
-}
-.welcome__Img {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background-size: cover;
-  background-position: center;
-  background-attachment: fixed;
-  position: relative;
-  z-index: 44;
-}
-.welcome__enrolBtn {
-  font-family: var(--Italianno);
-  margin-bottom: 1rem;
-  padding: 0.4em 1.4em;
-  font-size: 2.2em;
-  text-align: center;
-  cursor: pointer;
-}
-a {
-  color: inherit;
-  text-decoration: none;
-}
-a:hover,
-a:active {
-  color: inherit;
-  text-decoration: underline;
-}
-.leftFire {
-  animation: fireworks1 0.2s ease-in-out alternate-reverse 20;
-  -webkit-animation: fireworks1 0.2s ease-in-out alternate-reverse 20;
-  animation-delay: 1s;
-}
-.rightFire {
-  animation: fireworks1 0.2s ease-in-out alternate-reverse 20;
-  -webkit-animation: fireworks1 0.2s ease-in-out alternate-reverse 20;
-  animation-delay: 1.1s;
-}
-@keyframes fireworks1 {
-  from {
-    opacity: 0;
-    transform: scale(0.5);
-  }
-  to {
-    opacity: 1;
-    transform: scale(1);
-  }
-}
+    #welcome__Section {
+        display: flex;
+        position: relative;
+        height: 100vh;
+    }
+    #welcomePara {
+        line-height: 20.5px;
+        color: rgb(51, 51, 51);
+    }
+    .welcome__Wrap {
+        display: flex;
+        top: 30px;
+        flex-direction: column;
+        background-image: linear-gradient(to bottom right, rgb(189, 235, 229), rgba(255, 255, 255, 0.493));
+        max-width: 45rem;
+        max-height: auto;
+        position: relative;
+        z-index: 45;
+        margin: 2em;
+        box-shadow: 1px 20px 25px rgba(0, 0, 0, 0.3);
+        border-radius: 30px;
+        border-radius: 20px;
+        -webkit-border-radius: 20px;
+        -moz-border-radius: 20px;
+        -ms-border-radius: 20px;
+        -o-border-radius: 20px;
+    }
+    .welcome__Content {
+        padding: 1rem;
+        box-shadow: 0px 2px 21px -1px rgba(0, 0, 0, 0.75), 0px 2px 21px -1px rgba(0, 0, 0, 0.75) inset;
+        border-radius: 20px;
+    }
+    .welcome__Heading {
+        font-family: var(--Italianno);
+        font-size: 4em;
+        text-align: center;
+        color: black;
+    }
+    .welcome__Tag {
+        font-family: var(--Italianno);
+        font-size: 2.5em;
+        text-align: center;
+        margin-top: -1.5rem;
+    }
+    .welcome__headder {
+        font-family: var(--Italianno);
+        font-size: 2.5em;
+        margin-top: 1rem;
+        color: black;
+    }
+    .welcome__benefits {
+        font-family: var(--Italianno);
+        font-size: 2em;
+        text-align: center;
+    }
+    .welcome__benefits p {
+        font-family: var(--Roboto);
+        color: var(--lightBlack);
+        font-size: 0.5em;
+        text-align: left;
+        padding: 0 1rem;
+        margin-top: 1rem;
+        line-height: 1.55rem;
+    }
+    .welcome__celebration {
+        display: flex;
+        text-align: center;
+        justify-content: center;
+    }
+    .welcome__celebration h3 {
+        font-family: var(--Italianno);
+        font-size: 2.2em;
+        text-align: center;
+    }
+    .welcome__Img {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        position: relative;
+        z-index: 44;
+    }
+    .welcome__enrolBtn {
+        font-family: var(--Italianno);
+        margin-bottom: 1rem;
+        padding: 0.4em 1.4em;
+        font-size: 2.2em;
+        text-align: center;
+        cursor: pointer;
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+    a:hover,
+    a:active {
+        color: inherit;
+        text-decoration: underline;
+    }
+    .leftFire {
+        animation: fireworks1 0.2s ease-in-out alternate-reverse 20;
+        -webkit-animation: fireworks1 0.2s ease-in-out alternate-reverse 20;
+        animation-delay: 1s;
+    }
+    .rightFire {
+        animation: fireworks1 0.2s ease-in-out alternate-reverse 20;
+        -webkit-animation: fireworks1 0.2s ease-in-out alternate-reverse 20;
+        animation-delay: 1.1s;
+    }
+    @keyframes fireworks1 {
+        from {
+            opacity: 0;
+            transform: scale(0.5);
+        }
+        to {
+            opacity: 1;
+            transform: scale(1);
+        }
+    }
 
-.welcome__celebration img {
-  margin: 0 0.3rem 0 0.4rem;
-}
+    .welcome__celebration img {
+        margin: 0 0.3rem 0 0.4rem;
+    }
 
-@media screen and (max-width: 600px) {
-  .welcome__Wrap {
-    margin: 0.125rem;
-    top: 12vh;
-  }
-  #welcome__Section {
-    display: flex;
-    position: relative;
-    height: 100vh;
-  }
-}
-@media screen and (max-width: 366px) {
-  .welcome__Wrap {
-    margin: 0.125rem;
-    top: 26vh;
-  }
-  #welcome__Section {
-    display: flex;
-    position: relative;
-    height: 100vh;
-  }
-}
-@media screen and (max-height: 600px) {
-  #welcome__Section {
-    height: 117vh;
-  }
-}
+    @media screen and (max-width: 600px) {
+        .welcome__Wrap {
+            margin: 0.125rem;
+            top: 12vh;
+        }
+        #welcome__Section {
+            display: flex;
+            position: relative;
+            height: 100vh;
+        }
+    }
+    @media screen and (max-width: 366px) {
+        .welcome__Wrap {
+            margin: 0.125rem;
+            top: 26vh;
+        }
+        #welcome__Section {
+            display: flex;
+            position: relative;
+            height: 100vh;
+        }
+    }
+    @media screen and (max-height: 600px) {
+        #welcome__Section {
+            height: 117vh;
+        }
+    }
 </style>
